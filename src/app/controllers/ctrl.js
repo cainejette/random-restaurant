0c4ee5c5572338040001c23d2db521bca4aa02d4
Caine Jette <caine@attackpattern.com>
2016-06-15 22:09:03 -0700

    Randomly select a restaurant, provide a link to it

--- src/app/controllers/ctrl.js
+++ src/app/controllers/ctrl.js
@@ -3,11 +3,13 @@
 angular.module('rando').controller('ctrl',
     ['$scope', 'api',
         function ($scope, api) {
-            $scope.restaurants = [];
-
+            // $scope.restaurants = [];
+            $scope.restaurant = {};
+            
             const getRestaurants = (address) => {
                 api.getRestaurants(address).then(data => {
-                    $scope.restaurants = data.businesses;
+                    // $scope.restaurants = data.businesses;
+                    $scope.restaurant = data.businesses[Math.floor(Math.random() * 20)]
                 });
             }
 
